17681fdad074cdde257f8d74aed625432d2c47c3
omidashouri <omidashouri@gmail.com>
2022-05-02 10:44:42 +0430

    RGB 5 finished : omidashouri : 010212

--- src/components_shoppingList/Item.js
+++ src/components_shoppingList/Item.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import {useDispatch} from "react-redux";
 
 
-function Item({item}){
-    return(
+function Item({item}) {
+
+    const [show, setShow] = useState('none');
+    const [newitem, setNewitem] = useState('');
+    const dispatch = useDispatch();
+
+    function edititem() {
+        setShow('inline');
+    }
+
+    function submit() {
+        dispatch({type: 'SUBMIT', payload: {olditem: item, newitem: newitem}});
+        setShow('none')
+        setNewitem('');
+    }
+
+    function deleteitem(item) {
+        dispatch({type: 'DELETE_ITEM', payload: item});
+    }
+
+    return (
         <div>
             <div className={'mt-5 row justify-content-center'}>
                 <div className={'col-md-8'}>
                     <h1 className={'text-black'}>{item}</h1>
-                    <div className={'col-md-4'}>
-                        <button className={'btn btn-primary md-2'}>Edit</button>
-                        <button className={'btn btn-danger md-2'}>Delete</button>
-                    </div>
+                </div>
+                <div className={'col-md-4'}>
+                    <button className={'btn btn-primary md-2'} onClick={edititem}>Edit</button>
+                    <button className={'btn btn-danger md-2'} onClick={()=>deleteitem(item)}>Delete</button>
+                </div>
+                <div className={'col-md-8'}>
+                    <input type={'text'} style={{display: show}} value={newitem}
+                           onChange={(e) => setNewitem(e.target.value)}/>
+                    <button style={{display: show}} onClick={submit} className={'btn btn-warning m-2'}>Submit</button>
                 </div>
             </div>
         </div>
--- src/components_shoppingList/cartReducer.js
+++ src/components_shoppingList/cartReducer.js
@@ -1,16 +1,31 @@
 import React from "react";
 
-const initialCart={
-    shopcount:[]
+const initialCart = {
+    shopcount: []
 }
 
-function cartReducer(state=initialCart,action){
+function cartReducer(state = initialCart, action) {
     switch (action.type) {
-        case 'ADD_ITEM' : return {
-            ...state,
-            shopcount: [...state.shopcount,action.payload]
-        }
-
+        case 'ADD_ITEM' :
+            return {
+                ...state,
+                shopcount: [...state.shopcount, action.payload]
+            };
+        case 'SUBMIT':
+            return {
+                ...state,
+                shopcount: state.shopcount.map((item) => {
+                    if (item == action.payload.olditem) {
+                        item = action.payload.newitem;
+                        return item
+                    }
+                })
+            };
+        case'DELETE_ITEM':
+            return {
+                ...state,
+                shopcount: state.shopcount.filter((task) => task != action.payload)
+            };
     }
     return state;
 }
