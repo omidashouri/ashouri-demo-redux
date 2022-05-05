e0142f7ce976af5ffa337302439ad66481fdc8de
omidashouri <omidashouri@gmail.com>
2022-05-05 15:49:43 +0430

    CPPW 4. Global CSS Setup finished: omidashouri : 010215

---
+++ src/components_6portfolioCss/App_portfolioCss.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './App_portfolioCss.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import {Routes, Route} from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/home";
+import Skills from "./containers/skills";
+import Resume from "./containers/resume";
+import Portfolio from "./containers/portfolio";
+import Contact from "./containers/contact";
+
+
+//npm install react-router-dom sass react-vertical-timeline-component rc-progress react-tsparticles tsparticles react-simple-animate
+function App_portfolioCss() {
+    return (
+        <div className={'App_portfolioCss'}>
+            {/*    particles js*/}
+
+            {/*    nav bar component*/}
+
+            {/*    main page */}
+            <Routes>
+                {/*    create all routes */}
+                <Route path={'/'} index element={<Home/>}/>
+                <Route path={'/about'} element={<About/>}/>
+                <Route path={'/skills'} element={<Skills/>}/>
+                <Route path={'/resume'} element={<Resume/>}/>
+                <Route path={'/portfolio'} element={<Portfolio/>}/>
+                <Route path={'/contact'} element={<Contact/>}/>
+
+            </Routes>
+        </div>
+    );
+}
+
+export default App_portfolioCss;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App_1payload from "./components_1payload/App_1payload";
-import App_2shoppingList from "./components_2shoppingList/App_2shoppingList";
-import App_5bootstrap from "./components_5bootstrap/App_5bootstrap";
-import App_3combinedReducer from "./components_3combinedReducer/App_3combinedReducer";
-import App_4persistRedux from "./components_4persistRedux/App_4persistRedux";
+import {BrowserRouter as Router} from "react-router-dom";
+import App_portfolioCss from "./components_6portfolioCss/App_portfolioCss";
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,8 +18,12 @@ root.render(
         {/*<App_2shoppingList/>*/}
         {/*<App_3combinedReducer/>*/}
         {/*<App_4persistRedux/>*/}
+        {/*<App_5bootstrap/>*/}
 
-        <App_5bootstrap/>
+        <Router>
+            <App_portfolioCss/>
+        </Router>
+
     </React.StrictMode>
 );
 
