ebdf8df84dedd33ee1ae0330ce40a7a8bd921463
omidashouri <omidashouri@gmail.com>
2022-05-06 13:26:43 +0430

    CPPW 6. Particles JS Implementation finished : omidashouri : 010216

--- src/components_6portfolioCss/App_portfolioCss.js
+++ src/components_6portfolioCss/App_portfolioCss.js
@@ -1,7 +1,8 @@
 import React from "react";
 import './App_portfolioCss.css';
-// import 'bootstrap/dist/css/bootstrap.min.css'
-import {Routes, Route} from "react-router-dom";
+import Particles from 'react-tsparticles'
+import {loadFull} from 'tsparticles'
+import {Routes, Route, useLocation} from "react-router-dom";
 import Home from "./containers/home";
 import About from "./containers/home";
 import Skills from "./containers/skills";
@@ -9,13 +10,29 @@ import Resume from "./containers/resume";
 import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
+import particlesConfig from "./helpers/particlesConfig";
 
 
 //npm install react-router-dom sass react-vertical-timeline-component rc-progress react-tsparticles tsparticles react-simple-animate react-icons
 function App_portfolioCss() {
+
+    const particlesInit = async (main) => {
+        await loadFull(main);
+    }
+
+    const location = useLocation();
+    const renderParticleJsIfCurrentPageIsHomePage = location.pathname === '/';
+
+
     return (
         <div className={'App_portfolioCss'}>
+
             {/*    particles js*/}
+            {
+                renderParticleJsIfCurrentPageIsHomePage &&
+                <Particles id={'particles'} options={particlesConfig} init={particlesInit}/>
+            }
+
 
             {/*    nav bar component*/}
             <Navbar/>
