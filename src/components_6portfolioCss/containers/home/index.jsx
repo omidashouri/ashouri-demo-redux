5d3e9711994f6a44860da1a84538e466075a5774
omidashouri <omidashouri@gmail.com>
2022-05-07 09:32:28 +0430

    CPPW 7. Home Page Section finished: omidashouri : 010217

--- src/components_6portfolioCss/containers/home/index.jsx
+++ src/components_6portfolioCss/containers/home/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import {FaFacebook,FaLinkedin,FaInstagram,FaTwitter} from 'react-icons/fa'
+import {Animate} from 'react-simple-animate'
 import './styles.scss'
 
-const Home = ()=>{
-    return(
+const Home = () => {
+    return (
         <section className={'home'} id={'home'}>
             <div className={"home__text-wrapper"}>
                 <h1>
@@ -11,6 +13,20 @@ const Home = ()=>{
                     Back End Developer.
                 </h1>
             </div>
+            <Animate play={true} duration={1.5} delay={1} start={{transform:'translateY(600px)'}} end={{transform:'translateX(0px)'}}>
+                <div className={'contact-me'}>
+                    <div className={'contact-me__buttons-wrapper'}>
+                        <button>Hire me</button>
+                        <a>Download resume</a>
+                    </div>
+                    <div className={'contact-me__socials-wrapper'}>
+                        <FaLinkedin size={32}/>
+                        <FaFacebook size={32}/>
+                        <FaTwitter size={32}/>
+                        <FaInstagram size={32}/>
+                    </div>
+                </div>
+            </Animate>
         </section>
     )
 }
