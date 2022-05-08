91fe62d5f4108db82f65c95acd22fd3374748a1e
omidashouri <omidashouri@gmail.com>
2022-05-08 23:25:54 +0430

    CPPW 10.2 About Me Section completed : omidashouri : 010218

--- src/components_6portfolioCss/containers/about/index.jsx
+++ src/components_6portfolioCss/containers/about/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import {BsInfoCircleFill} from 'react-icons/bs'
+import {personalData} from "./utils";
+import {DiApple, DiAndroid} from 'react-icons/di'
+import {FaDev, FaDatabase} from 'react-icons/fa'
+import './styles.scss'
 
 const About = () => {
     return (
@@ -9,6 +13,40 @@ const About = () => {
                 headerText={'About Me'}
                 icon={<BsInfoCircleFill size={40}/>}
             />
+            <div className={'about__content'}>
+                <div className={'about__content__personalWrapper'}>
+                    <h3 className={'developerContent'}>BackEnd Developer</h3>
+                    <p>Job Profile</p>
+                    <h3 className={'personalContent'}>Personal Information</h3>
+                    <ul>
+                        {
+                            personalData.map((item, key) => (
+                                    <li key={key}>
+                                        <span className={'title'}>{item.label}</span>
+                                        <span className={'value'}>{item.value}</span>
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
+                </div>
+                <div className={'about__content__servicesWrapper'}>
+                    <div className={'about__content__servicesWrapper__innerContent'}>
+                        <div>
+                            <DiApple size={60} color={'var(--selected-theme-main-color)'}/>
+                        </div>
+                        <div>
+                            <FaDatabase size={60} color={'var(--selected-theme-main-color)'}/>
+                        </div>
+                        <div>
+                            <DiAndroid size={60} color={'var(--selected-theme-main-color)'}/>
+                        </div>
+                        <div>
+                            <FaDev size={60} color={'var(--selected-theme-main-color)'}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
---
+++ src/components_6portfolioCss/containers/about/utils.js
@@ -0,0 +1,26 @@
+
+export const personalData =
+    [
+        {
+            label : 'Name',
+            value: 'Omid Ashouri'
+        },
+        {
+            label : 'Age',
+            value: '40'
+        },
+        {
+            label : 'Address',
+            value: 'Iran, Tehran'
+        },
+        {
+            label : 'Email:',
+            value: 'omidashouri@gmail.com'
+        },
+        {
+            label : 'Contact No:',
+            value: '+9809126607350'
+        }
+
+
+    ]
