17ebd59d673e53db7b0556caee2aa03ef6c176bd
omidashouri <omidashouri@gmail.com>
2022-05-10 12:42:22 +0430

    CPPW 12.1 Portfolio Section complete: omidashouri : 010220

--- src/components_6portfolioCss/containers/portfolio/index.jsx
+++ src/components_6portfolioCss/containers/portfolio/index.jsx
@@ -1,12 +1,45 @@
 import React from "react";
+import ImageOne from '../../images/1.jpeg';
+import ImageTwo from '../../images/2.jpeg';
+import ImageThree from '../../images/3.jpeg';
+import ImageFour from '../../images/4.jpeg';
+import ImageFive from '../../images/5.jpeg';
+import PageHeaderContent from "../../components/pageHeaderContent";
+import {AiFillProject} from 'react-icons/ai'
+import {filterOptions, portfolioData} from "./utils";
 
 
-const Portfolio = ()=>{
-    return(
-        <div>
-            Portfolio
-        </div>
-    )
+const Portfolio = () => {
+    return (
+        <section id={'portfolio'} className={'portfolio'}>
+            <PageHeaderContent headerText={'My Portfolio'} icon={<AiFillProject size={40}/>}/>
+            <div className={'portfolio__content'}>
+                <ul className={'portfolio__content__filter'}>
+                    {
+                        filterOptions.map((option) =>
+                            (
+                                <li key={option.id}>{option.label}</li>
+                            )
+                        )
+                    }
+                </ul>
+                <div className={'portfolio__content__cards'}>
+                    {
+                        portfolioData.map((item,key) =>
+                            (
+                                <div key={key} className={'portfolio__content__cards__item'}>
+                                    <div className={'portfolio__content__cards__item__image-wrapper'}>
+                                        {/*<a>*/}
+                                            <img src={item.image} alt={'project image'}/>
+                                        {/*</a>*/}
+                                    </div>
+                                </div>
+                            ))
+                    }
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Portfolio;
---
+++ src/components_6portfolioCss/containers/portfolio/utils.js
@@ -0,0 +1,55 @@
+import ImageOne from '../../images/1.jpeg';
+import ImageTwo from '../../images/2.jpeg';
+import ImageThree from '../../images/3.jpeg';
+import ImageFour from '../../images/4.jpeg';
+import ImageFive from '../../images/5.jpeg';
+
+export const portfolioData =
+    [
+        {
+            sectionId: 2,
+            projectName: 'ToDo Application',
+            projectLink: '',//desire link
+            image: ImageOne
+        },
+        {
+            sectionId: 2,
+            projectName: 'Shopping Cart Application',
+            projectLink: '',//desire link
+            image: ImageTwo
+        },
+        {
+            sectionId: 2,
+            projectName: 'ToDo Application',
+            projectLink: '',//desire link
+            image: ImageThree
+        },
+        {
+            sectionId: 3,
+            projectName: 'Supplier Design App',
+            projectLink: '',//desire link
+            image: ImageFour
+        },
+        {
+            sectionId: 3,
+            projectName: 'Design Application',
+            projectLink: '',//desire link
+            image: ImageFive
+        }
+    ];
+
+export const filterOptions =
+    [
+        {
+            label: 'All',
+            id: 1
+        },
+        {
+            label: 'Development',
+            id: 2
+        },
+        {
+            label: 'Design',
+            id: 3
+        }
+    ]
