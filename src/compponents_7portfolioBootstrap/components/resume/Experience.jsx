73075fc848599249fbca7f589d9a414c94fcfc23
omidashouri <omidashouri@gmail.com>
2022-06-05 00:59:45 +0430

    PB_13_Adjustment_And_Correction, complete  : omidashouri : 010315

--- src/compponents_7portfolioBootstrap/components/resume/Experience.jsx
+++ src/compponents_7portfolioBootstrap/components/resume/Experience.jsx
@@ -2,163 +2,166 @@ import React from "react";
 
 const Experience = () => {
     return (
-        <div id={'resume'} className={'container'}>
-            <div className="row d-flex">
+        <section className='resume-main-wrapper container-fluid'>
+            <div id={'resume'} className={'container'}>
+                <h1>resume</h1>
 
-                <div className="col-lg-6 col-md-8 col-sm-12">
-                    <div className="resume">
-                        <div className="my-5">
-                            <h1>Education</h1>
-                        </div>
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>Treehouse</h3>
-                                    <h5>HTML</h5>
-                                    <h6>2023</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
+                <div className="row d-flex">
+                    <div className="col-lg-6 col-md-8 col-sm-12">
+                        <div className="resume">
+                            <div className="my-5">
+                                <h1>Education</h1>
+                            </div>
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>Treehouse</h3>
+                                        <h5>HTML</h5>
+                                        <h6>2023</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>W3Schools</h3>
-                                    <h5>CSS</h5>
-                                    <h6>2024</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>W3Schools</h3>
+                                        <h5>CSS</h5>
+                                        <h6>2024</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>Udemy</h3>
-                                    <h5>Javascript</h5>
-                                    <h6>2025</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>Udemy</h3>
+                                        <h5>Javascript</h5>
+                                        <h6>2025</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>Sckillshare</h3>
-                                    <h5>ReactJS</h5>
-                                    <h6>2025</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {/* RIGHT COLOMN */}
-
-                <div className='col-lg-6 col-md-8 col-sm-12'>
-                    <div className='resume'>
-                        <div className='my-5'>
-                            <h1>Experience</h1>
-                        </div>
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>Freak Company</h3>
-                                    <h5>HTML</h5>
-                                    <h6>2022-2023</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>Freak Company</h3>
-                                    <h5>CSS</h5>
-                                    <h6>2023-2024</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>Freak Company</h3>
-                                    <h5>Javascript</h5>
-                                    <h6>2024-2025</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='container wrapper'>
-                            <div className='timeline-block'>
-                                <div className='marker'></div>
-                                <div className='timeline-content'>
-                                    <h3>Freak Company</h3>
-                                    <h5>ReactJS</h5>
-                                    <h6>2025-2026</h6>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
-                                        provident, doloremque adipisci molestias iure, ut natus
-                                        exercitationem consectetur, facilis id recusandae odio ea!
-                                    </p>
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>Sckillshare</h3>
+                                        <h5>ReactJS</h5>
+                                        <h6>2025</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
+                    {/* RIGHT COLOMN */}
+
+                    <div className='col-lg-6 col-md-8 col-sm-12'>
+                        <div className='resume'>
+                            <div className='my-5'>
+                                <h1>Experience</h1>
+                            </div>
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>Freak Company</h3>
+                                        <h5>HTML</h5>
+                                        <h6>2022-2023</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>Freak Company</h3>
+                                        <h5>CSS</h5>
+                                        <h6>2023-2024</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>Freak Company</h3>
+                                        <h5>Javascript</h5>
+                                        <h6>2024-2025</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='container wrapper'>
+                                <div className='timeline-block'>
+                                    <div className='marker'></div>
+                                    <div className='timeline-content'>
+                                        <h3>Freak Company</h3>
+                                        <h5>ReactJS</h5>
+                                        <h6>2025-2026</h6>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                            Veniam quia incidunt doloribus? Corporis ipsa nesciunt iusto
+                                            provident, doloremque adipisci molestias iure, ut natus
+                                            exercitationem consectetur, facilis id recusandae odio ea!
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
